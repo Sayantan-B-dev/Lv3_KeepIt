8e68cb0c03b53e95e47d4b122c7e2db549d2f49c
Sayantan <sayantanbharati611@gmail.com>
2025-08-21 00:39:06 +0530

    server port fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import passport from "passport"
 // Set development environment
 process.env.NODE_ENV = 'development'
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.DATABASE_URL)
     .then(() => {
         console.log("DB Connected!!")
