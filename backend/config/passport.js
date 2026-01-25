be5a4465a9e3bda8acc1ec57b5042e86e2589d9f
Sayantan <sayantanbharati611@gmail.com>
2026-01-25 19:40:37 +0530

    rediction fix for google auth2

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -48,7 +48,7 @@ passport.use(new LocalStrategy({
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/api/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
     passReqToCallback: true
 }, async (req, accessToken, refreshToken, profile, done) => {
     try {
